3a0d04f6a9f3f25461cea18e85ef97619e627b16
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-04-06 06:38:13 -0400

    feat: Allows for the game to create a board that track players move

--- connect4.js
+++ connect4.js
@@ -1,11 +1,23 @@
 // Players p1 and p2 alternate turn. On each turn, a price is dropped into a column until a player get four in arow or the board is full.
 
 class Connect4Game {
-	constructor(p1, p2, height, width) {
+	constructor(p1 = 'js', p2 = 'tk', height = 6, width = 7) {
 		this.players = [p1, p2];
 		this.height = height;
 		this.width = width;
 		this.board = [];
-		this.currPlayer = p1;
+		this.makeBoard();
+	}
+
+	makeBoard() {
+		for (let row = 0; row < this.height; row++) {
+			let newRow = [];
+			for (let x = 0; x < this.width; x++) {
+				newRow.push('');
+			}
+			this.board.push(newRow);
+		}
 	}
 }
+
+const game = new Connect4Game();
