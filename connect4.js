5800dfe1fe3108ddf20947cdbcd4e91d676c3d86
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-04-06 07:33:06 -0400

    feat: Created playing board and adds event listener to the top row

--- connect4.js
+++ connect4.js
@@ -9,6 +9,7 @@ class Connect4Game {
 		this.makeBoard();
 	}
 
+	// makeBoard: create game board structure. board = array of rowws, each row is an array of cells.
 	makeBoard() {
 		for (let row = 0; row < this.height; row++) {
 			let newRow = [];
@@ -16,6 +17,36 @@ class Connect4Game {
 				newRow.push('');
 			}
 			this.board.push(newRow);
+		}
+	}
+
+	// makeHtmlBoard: creates html table with height x width
+	makeHtmlBoard() {
+		const htmlBoard = document.getElementById('board');
+
+		// create column-top and added initial player class
+		const topRow = document.createElement('tr');
+		topRow.setAttribute('id', 'column-top');
+		topRow.classList.add('p1');
+		topRow.addEventListener('click', handleClick);
+
+		// creates column-top row where players drop piece
+		for (let x = 0; x < this.width; x++) {
+			const dropPieceColumn = document.createElement('td');
+			dropPieceColumn.setAttribute('id', x);
+			topRow.append(dropPieceColumn);
+		}
+		htmlBoard.append(topRow);
+
+		// creates the main part of the board
+		for (let y = 0; y < this.height; y++) {
+			const row = document.createElement('tr');
+			for (let x = 0; x < this.width; x++) {
+				const cell = document.createElement('td');
+				cell.setAttribute('id', `${y}-${x}`);
+				row.append(cell);
+			}
+			htmlBoard.append(row);
 		}
 	}
 }
